0abd95e21b980f18f93b99e88a6ac013653591ed
zlmtk <guillaume.irix@gmail.com>
2017-10-29 14:02:48 +0100

    Add simple bash to reload container is needed

--- tex-env.sh
+++ tex-env.sh
@@ -1,5 +1,29 @@
- 
-xhost +
-docker run -d  -v /tmp/.X11-unix:/tmp/.X11-unix -v /$(pwd):/workspace  -e DISPLAY=$DISPLAY zlmtk/texmaker-env texmaker
+#!/bin/bash 
+########################################################
+#Use this script to reload the same container exited
+#if needed. 
+#usage : 
+#./tex-env.sh
+########################################################
 
+#Provide Xserver access 
+xhost local:root
+
+#Get container states
+is_exited="$(docker ps -aq -f status=exited -f name=texmaker-apps)"
+is_running="$(docker ps -q -f name=texmaker-apps)"
+
+
+ if [ "${is_exited}" ]; then
+
+  echo "Restart texmaker container" 
+  docker start  ${is_exited}
+
+
+elif [ ! "${is_running}"  ]; then 
+
+  echo "Start new texmaker container" 
+  docker run -d  -v /tmp/.X11-unix:/tmp/.X11-unix -v /$(pwd):/workspace  --name texmaker-apps -e DISPLAY=$DISPLAY zlmtk/texmaker-env texmaker
+
+fi
 
